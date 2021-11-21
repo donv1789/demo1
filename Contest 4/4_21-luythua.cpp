b8ea727594cbe171bef91e0c99e08ba527b1a241
Độ Nguyễn Văn <donv1789@gmail.com>
2021-11-21 15:38:41 +0700

    b

--- Contest 4/4_21-luythua.cpp
+++ Contest 4/4_21-luythua.cpp
@@ -1,22 +1,19 @@
 #include<iostream>
 using namespace std;
-const long Max = 1e9 + 7;
-
-int luythua(int n,long k){
-	if(k==1) return n;
-	long x=luythua(n,k/2); // VD n=2, k=3 thi k/2=1 va x=2
-	if(k%2==0) return (x*x)%Max;
-	return ((n*x)%Max*x)%Max;// 3%2=1.tra ve 2*2*2=8
+const int mod =1e9 + 7;
+long long lt(long a,long b){
+	if(b==0) return 1;
+	long long x = lt(a,b/2);
+	if(b%2==0) return (x*x)%mod;
+	return a*(x*x%mod)%mod;
 }
-
 int main(){
 	ios_base::sync_with_stdio(false);
-
-	for(int i=0 ; i<20;i++){
-		int a;
-		long b;
+	int a;
+	long b;
+	for(int i=0;i<19;i++){
 		cin>>a>>b;
-		if(a==0 && b==0)return 0;
-		cout<<luythua(a,b)<<endl;
+		if(a==0 && b==0) return 0;
+		cout<<lt(a,b)<<endl;
 	}
 }
